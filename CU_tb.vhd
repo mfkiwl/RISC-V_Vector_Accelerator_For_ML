cc884731320fea82e2fde7f76c3aa41909e4584c
Iska1999 <62153324+Iska1999@users.noreply.github.com>
2020-03-13 19:33:41 +0200

    Update CU_tb.vhd

--- CU_tb.vhd
+++ CU_tb.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/29/2020 11:01:30 AM
--- Design Name: 
--- Module Name: Control_Unit_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 

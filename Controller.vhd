c436a513d8d2c0f198c79592e17e18a1a220cc0a
Iska1999 <62153324+Iska1999@users.noreply.github.com>
2020-03-13 19:34:40 +0200

    Update Controller.vhd

--- Controller.vhd
+++ Controller.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/29/2020 11:22:56 AM
--- Design Name: 
--- Module Name: Top_Level - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 

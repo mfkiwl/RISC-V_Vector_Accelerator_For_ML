a494f3bf782e4f37e71030ed1b81e95c3f2d783c
Iska1999 <62153324+Iska1999@users.noreply.github.com>
2020-03-13 19:33:03 +0200

    Update Bank1.vhd

--- Bank1.vhd
+++ Bank1.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/18/2020 10:41:49 AM
--- Design Name: 
--- Module Name: Bank1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;

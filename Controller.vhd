0cb9bc7c127e00febf14489b93d8233cf9f4d909
Iska1999 <62153324+Iska1999@users.noreply.github.com>
2020-03-14 15:44:28 +0200

    Update Controller.vhd

--- Controller.vhd
+++ Controller.vhd
@@ -1,15 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Controller is
 
     generic (
